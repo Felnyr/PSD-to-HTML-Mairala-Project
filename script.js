8cbcc60872e12f7e4d9ac5bda2d5a400e212522d
Felnyr <pawlello@gmail.com>
2017-03-14 23:44:59 +0100

    First commit

---
+++ script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $('.logo').on('click', function(){
+      $('html, body').animate({ scrollTop: 0 }, 'slow');
+    });
+    
+    
+
+    $('.btn-mobile').on('click',function(){
+      if($('.nav-top').hasClass('responsive')){
+        $('.nav-top').removeClass('responsive')
+        c
+      }else{
+        $('.nav-top').addClass('responsive')
+      }
+    });
+});
