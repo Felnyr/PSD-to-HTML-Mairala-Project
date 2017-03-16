6e46a0ea09abf6f9a87e8136d9843ce3b76b0b02
Felnyr <pawlello@gmail.com>
2017-03-16 19:41:19 +0100

    Added bootstrap carousel to ct-clients

--- script.js
+++ script.js
@@ -2,9 +2,7 @@ $(document).ready(function(){
     $('.logo').on('click', function(){
       $('html, body').animate({ scrollTop: 0 }, 'slow');
     });
-    
-    
-
+  
     $('.btn-mobile').on('click',function(){
       if($('.nav-top').hasClass('responsive')){
         $('.nav-top').removeClass('responsive')
@@ -13,4 +11,8 @@ $(document).ready(function(){
         $('.nav-top').addClass('responsive')
       }
     });
+  
+    $('.carousel').carousel({
+      interval: 2000
+    })
 });
